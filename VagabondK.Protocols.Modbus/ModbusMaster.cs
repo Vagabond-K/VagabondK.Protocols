927f1d9093f1f59c033e0586ef313351a025f87a
Vagabond K <vagabond-k@naver.com>
2021-12-23 16:02:36 +0900

    요청 전 잔여 수신 메시지가 있을 경우에 대한 처리 추가

--- VagabondK.Protocols.Modbus/ModbusMaster.cs
+++ VagabondK.Protocols.Modbus/ModbusMaster.cs
@@ -147,7 +147,8 @@ namespace VagabondK.Protocols.Modbus
 
             if (!(serializer is ModbusTcpSerializer))
             {
-                channel.ReadAllRemain().ToArray();
+                var remainMessage = channel.ReadAllRemain().ToArray();
+                serializer.RaiseUnrecognized(channel, remainMessage);
             }
 
             channel.Write(requestMessage);
